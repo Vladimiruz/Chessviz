aafa57ded5b59070b661108128dd7de544028a88
Vladimiruz <wvowva2001@mail.ru>
2020-05-31 19:28:49 +0700

    Complete test cover

--- test/test.cpp
+++ test/test.cpp
@@ -54,6 +54,94 @@ TEST(Rook, WhiteRook_invalid)
     EXPECT_EQ(expected, result);
 }
 
+TEST(Bishop, WhiteBishop_valid)
+{
+    board[6][4]=' ';
+    int x1 = 7, x2 = 5;
+    int y1 = 3, y2 = 5;
+    team = 0;
+    int result = MoveCheck(x1, x2, y1, y2);
+    int expected = 1;
+    EXPECT_EQ(expected, result);
+}
+
+TEST(Bishop, WhiteBishop_invalid)
+{
+    board[6][4]='P';
+    int x1 = 7, x2 = 5;
+    int y1 = 3, y2 = 5;
+    team = 0;
+    int result = MoveCheck(x1, x2, y1, y2);
+    int expected = 0;
+    EXPECT_EQ(expected, result);
+
+}
+
+TEST(Knight, WhiteKnight_valid)
+{
+    int x1 = 7, x2 = 5;
+    int y1 = 2, y2 = 3;
+    team = 0;
+    int result = MoveCheck(x1, x2, y1, y2);
+    int expected = 1;
+    EXPECT_EQ(expected, result);
+}
+
+TEST(Knight, WhiteKnight_invalid)
+{
+    int x1 = 7, x2 = 6;
+    int y1 = 2, y2 = 4;
+    team = 0;
+    int result = MoveCheck(x1, x2, y1, y2);
+    int expected = 0;
+    EXPECT_EQ(expected, result);
+}
+
+TEST(Queen, WhiteQueen_valid)
+{
+    board[6][3]=' ';
+    int x1 = 7, x2 = 5;
+    int y1 = 4, y2 = 2;
+    team = 0;
+    int result = MoveCheck(x1, x2, y1, y2);
+    int expected = 1;
+    EXPECT_EQ(expected, result);
+}
+
+TEST(Queen, WhiteQueen_invalid)
+{
+    board[6][3]='P';
+    int x1 = 7, x2 = 5;
+    int y1 = 4, y2 = 2;
+    team = 0;
+    int result = MoveCheck(x1, x2, y1, y2);
+    int expected = 0;
+    EXPECT_EQ(expected, result);
+
+}
+TEST(King, WhiteKing_valid)
+{
+    board[6][5]=' ';
+    int x1 = 7, x2 = 6;
+    int y1 = 5, y2 = 5;
+    team = 0;
+    int result = MoveCheck(x1, x2, y1, y2);
+    int expected = 1;
+    EXPECT_EQ(expected, result);
+}
+
+TEST(King, WhiteKing_invalid)
+{
+    board[6][5]='P';
+    int x1 = 7, x2 = 6;
+    int y1 = 5, y2 = 5;
+    team = 0;
+    int result = MoveCheck(x1, x2, y1, y2);
+    int expected = 0;
+    EXPECT_EQ(expected, result);
+
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
